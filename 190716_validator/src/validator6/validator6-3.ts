f6954bccf9aba18c180316e4129cec3eef5ea794
Nick Kelly <nick.kelly314@gmail.com>
2019-07-21 21:15:32 +1000

    misc

--- 190716_validator/src/validator6/validator6-3.ts
+++ 190716_validator/src/validator6/validator6-3.ts
@@ -2,3 +2,4 @@ import { Either, right, left } from 'fp-ts/lib/Either';
 
 const isString = (s: unknown): Either<string, string> => (typeof s === 'string' ? right(s) : left('must be a string'));
 const minLength = (s: string, )
+g
