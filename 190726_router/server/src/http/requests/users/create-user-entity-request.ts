85c250d1a4e0b0ac3c03d97d5180b98631c40c43
NickKelly1 <nick.kelly314@gmail.com>
2019-07-28 16:10:32 +1000

    validator work

--- 190726_router/server/src/http/requests/users/create-user-entity-request.ts
+++ 190726_router/server/src/http/requests/users/create-user-entity-request.ts
@@ -1,27 +1,63 @@
 import { UserEntityAttributes, UserEntity } from '../../../data/entities/user-entity';
 import { ContainsWords } from '../../../validation/contains-words';
 import { sequenceT } from 'fp-ts/lib/Apply';
-import { getValidation, right, left, Either, map } from 'fp-ts/lib/Either';
+import { getValidation, right, left, Either, map, chain, isRight, isLeft } from 'fp-ts/lib/Either';
 import { getSemigroup, NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { AStringKeyOf } from '../../../../@types/helpers/helper-types';
 
 // ContainsWords.validate();
 
+// @see https://dev.to/gcanti/getting-started-with-fp-ts-either-vs-validation-5ejaj
+
+const minLength = (s: string): Either<NonEmptyArray<string>, string> => (s.length > 5 ? right(s) : left(['too short']));
+const hasA = (s: string): Either<NonEmptyArray<string>, string> => (s.includes('a') ? right(s) : left(["doesn't have a"]));
+const hasB = (s: string): Either<NonEmptyArray<string>, string> => (s.includes('b') ? right(s) : left(["doesn't have b"]));
+
+const applicativeValidation = getValidation(getSemigroup<string>());
+
 export class CreateUserEntityRequest {
-  public static validate = (requestBody: Partial<Omit<UserEntityAttributes, UserEntity['primaryKey']>>) => {
-    const validateName = (s: string) => {
-      const z = pipe(
-        sequenceT(getValidation(getSemigroup<string>()))(
-          (s: string) => (s.length > 5 ? right(s) : left(s)),
-          ContainsWords.validate(s),
-        ),
-        map(() => s),
-      );
-    };
+  public static validate = (
+    requestBody: Partial<Record<AStringKeyOf<Omit<UserEntityAttributes, UserEntity['primaryKey']>>, unknown>>,
+  ) => {
+    //
+  };
 
-    if (!requestBody.name) throw new TypeError('oi!');
+  private static validateUser = (u: unknown) => {
+    // TODO
+    return pipe(sequenceT(applicativeValidation)(this.fullValidateName));
+  };
 
-    validateName(requestBody.name);
+  public static fullValidateName = (u: unknown) => {
+    const isString = pipe(
+      u !== undefined ? right(u) : left(['TODO <fieldname> must exist...']),
+      chain(u => (typeof u === 'string' ? right(u) : left(['TODO <fieldname> must be a string...']))),
+    );
+
+    if (isLeft(isString)) return isString;
+
+    const s = isString.right;
+
+    return pipe(
+      sequenceT(getValidation(getSemigroup<string>()))(minLength(s), hasA(s), hasB(s)),
+      // if no errors, simply return s
+      map(() => s),
+    );
+  };
+
+  public static validateIsString = (u: unknown) => {
+    return pipe(
+      u !== undefined ? right(u) : left(['TODO <fieldname> must exist...']),
+      chain(u => (typeof u === 'string' ? right(u) : left(['TODO <fieldname> must be a string...']))),
+    );
+  };
+
+  public static validateName = (s: string) => {
+    return pipe(
+      sequenceT(getValidation(getSemigroup<string>()))(minLength(s), hasA(s), hasB(s)),
+      // if no errors, simply return s
+      map(() => s),
+    );
   };
 
   // public static sanitize = ([])
@@ -32,3 +68,18 @@ export class CreateUserEntityRequest {
     this.body = body;
   }
 }
+
+const frst = 'frst';
+const second = 'second';
+const thirda = 'thirda';
+const fourthb = 'fourthb';
+const fifthab = 'fifthab';
+
+console.log('-------------------------------------------------');
+console.log('----------------- validateName ------------------');
+console.log(frst, CreateUserEntityRequest.fullValidateName(frst));
+console.log(second, CreateUserEntityRequest.fullValidateName(second));
+console.log(thirda, CreateUserEntityRequest.fullValidateName(thirda));
+console.log(fourthb, CreateUserEntityRequest.fullValidateName(fourthb));
+console.log(fifthab, CreateUserEntityRequest.fullValidateName(fifthab));
+console.log(fifthab, CreateUserEntityRequest.fullValidateName(undefined));
