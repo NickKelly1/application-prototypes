a3df24ef6e276311c1cc10058cba24a745cb46f9
Nick Kelly <nick.kelly314@gmail.com>
2019-08-03 23:40:21 +1000

    rxjs

---
+++ 190803_rxjs/client/.eslintrc.js
@@ -0,0 +1,28 @@
+// https://dev.to/benweiser/how-to-set-up-eslint-typescript-prettier-with-create-react-app-3675
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'eslint:recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    jasmine: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'prettier/prettier': ['error', { singleQuote: true }],
+    '@typescript-eslint/camelcase': 0,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
