32c6fa46c6bc255a5454c9a4183139cc8895be0a
Nick Kelly <nick.kelly314@gmail.com>
2019-08-10 18:45:19 +1000

    copy helpers around

---
+++ 190809_graphql_apollo_1/server/src/utils.ts
@@ -0,0 +1,68 @@
+import { Sequelize, Op, INTEGER, DATE, STRING } from 'sequelize';
+
+export function paginateResults({
+  after: afterCursor,
+  pageSize = 20,
+  results,
+  // can pass in a function to calculate an item's cursor
+  getCursor = () => null,
+}: {
+  after: unknown;
+  pageSize: number;
+  results: { cursor?: unknown }[];
+  getCursor: Function;
+}) {
+  //
+  if (pageSize < 1) return [];
+
+  if (!afterCursor) return results.slice(0, pageSize);
+
+  const cursorIndex = results.findIndex(item => {
+    let itemCursor = item.cursor ? item.cursor : getCursor(item);
+    return itemCursor ? afterCursor === itemCursor : false;
+  });
+
+  if (cursorIndex <= 0) return results.slice(0, pageSize);
+  // don't allow overflow
+  if (cursorIndex >= results.length - 1) return [];
+  return results.slice(cursorIndex + 1, Math.min(results.length, cursorIndex + 1 + pageSize));
+}
+
+export function createStore() {
+  const operatorsAliases = { $in: Op.in };
+
+  const db = new Sequelize('database', 'username', 'password', {
+    dialect: 'sqlite',
+    storage: './store.sqlite',
+    operatorsAliases,
+    logging: true,
+  });
+
+  const users = db.define('user', {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    createdAt: DATE,
+    updatedAt: DATE,
+    email: STRING,
+    token: STRING,
+  });
+
+  const trips = db.define('trip', {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    createdAt: DATE,
+    updatedAt: DATE,
+    email: STRING,
+    token: STRING,
+  });
+
+  return { users, trips };
+}
+
+export type Store = ReturnType<typeof createStore>;
