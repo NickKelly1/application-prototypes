abc3a1af4a9a26e637044b19ff4e257d7f3b005e
Nick Kelly <nick.kelly314@gmail.com>
2019-08-19 19:42:56 +1000

    uncommited work

--- 190707_ts_templates/client/.eslintrc.js
+++ 190707_ts_templates/client/.eslintrc.js
@@ -1,25 +1,36 @@
 // https://dev.to/benweiser/how-to-set-up-eslint-typescript-prettier-with-create-react-app-3675
+// https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
 module.exports = {
-  parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
   ],
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  env: {
-    es6: true,
-    browser: true,
-    jasmine: true,
-    jest: true,
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'jest', 'jsx-a11y'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019, // allows for parsing of modern ECMAScript features
+    sourceType: 'module',
+    ecmaFeatures: {},
+    // 'project': './tsconfig.json',
+  },
+  env: { es6: true, browser: true, jest: true },
+  globals: {
+    // window,
+    // document,
+    // fetch,
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    'prettier/prettier': ['error', { singleQuote: true }],
     '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
     'no-console': 0,
+    '@typescript-eslint/indent': ['error', 2],
+    // https://www.npmjs.com/package/eslint-plugin-react-hooks
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'quotes': ['error', 'single',],
   },
   settings: {
     react: {
