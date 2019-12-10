bb64f86a5659c3e9b6feba84f036489e06f5c4c6
Nick Kelly <nick.kelly314@gmail.com>
2019-12-11 07:50:50 +1100

    env generation

--- 191208_microservices_3/reset-docker-compose-development-env.sh
+++ 191208_microservices_3/reset-docker-compose-development-env.sh
@@ -1,11 +1,10 @@
 echo 'Deleting .env file and replacing with each services concatenated .env file...'
 
 # clear .env
-echo "
-**************************
-**    GENERATED FILE    **
-**      DO NOT EDIT     **
-**************************
+echo "# **************************
+# **    GENERATED FILE    **
+# **      DO NOT EDIT     **
+# **************************
 " > .env
 
 cat services/user-service/.env >> .env
@@ -15,17 +14,22 @@ echo "Pruning shared environment variables"
 # https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
 # https://stackoverflow.com/questions/7657647/combining-two-sed-commands
 # https://gist.github.com/NickKelly1/87bd871b21082923d2f2a780c516ee52
-sed -i.bak \
-  "/s/REDIS_HOST//d" \
-  "/s/REDIS_PORT//d" \
-  "/s/REDIS_PASSWORD//d" \
-  .env
+sed -i "/s/REDIS_HOST//d" .env
+sed -i "/s/REDIS_PORT//d" .env
+sed -i "/s/REDIS_PASSWORD//d" .env
 
 echo "Re-adding (once) shared environment variables"
 
 # https://hub.docker.com/r/bitnami/redis/
 echo "
+
+
+# ******************
+# ***** SHARED *****
+# ******************
+
 REDIS_HOST=localhost
 REDIS_PORT=6379
 REDIS_PASSWORD=password123
-"
+
+" >> .env
