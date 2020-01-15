5c4fe985e30f6b3cbea48fdc1266e287e7643064
Nick Kelly <nick.kelly314@gmail.com>
2020-01-16 00:54:08 +1100

    rename packages

--- 200115_ts_composite_5_my_test_2/packages/back-end/src/index.ts
+++ 200115_ts_composite_5_my_test_2/packages/back-end/src/index.ts
@@ -1,3 +1,3 @@
-import { sharedLogger } from '@composite/shared';
+import { sharedLogger } from '@nick-kelly/ts_composite_5__shared';
 
 sharedLogger('hello world :) this is being called from back-end/src/index.ts');
